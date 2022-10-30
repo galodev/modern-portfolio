dbeb164f71694eb8ecd7df30c4b670caf82850b0
Galo Vega <contacto.detodounpocomx@gmail.com>
2022-10-30 17:56:24 -0600

    new modal effect

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const modal = document.getElementById("modal")
+const modalInput = document.getElementById("modal-input")
+const submitBtn = document.getElementById("submit-name-btn")
+
+
+setTimeout(function(){
+    modalInput.style.display= "flex"
+},8000)
+
+submitBtn.addEventListener("click", function(){
+
+})

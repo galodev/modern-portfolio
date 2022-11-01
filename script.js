6c69f5d084a5dabd3ef7e2ceb3a29463ebba083c
Galo Vega <contacto.detodounpocomx@gmail.com>
2022-11-01 13:52:52 -0600

    solved some bugs in the modal

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-
 const modal = document.getElementById("modal")
 const modalInput = document.getElementById("modal-input")
 const submitBtn = document.getElementById("submit-name-btn")
 
-const modalForm = document.getElementById("modal-form")
 
+const modalForm = document.getElementById("modal-form")
+const userName = consentFormData.get('userName')
+
+// What im looking here is to take the value that the user put on name and add to the site, when the user clicks submit the modal will be closed
 
 setTimeout(function(){
     modalInput.style.display= "flex"
 },8000)
 
-submitBtn.addEventListener("click", function(){
-
-})
-
 modalForm.addEventListener('submit', function(e){
     e.preventDefault()
-    const loginFormData = new FormData (modalForm)
+    const loginFormData = new FormData(modalForm)
+    const name = loginFormData.get("userName")
+    modal.style.display = "none"
 })
+
+
+setTimeout(function(){
+    document.getElementById("main-info").innerHTML = `
+    <h1>Hi! <span class="modal-display-name"> ${userName} </span> <br> <span>My name is Galo</span> </h1>
+`
+}, 1000)
